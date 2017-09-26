9bb7f6de15f3cb77ced2ab37f1f64c72bd3fc8d7
baroquenroll <johannsuggestionbachs@runbox.com>
2017-09-25 22:24:02 -0400

    Fix false positive for user's own posts
    Also improve dashboard URL match and tweak selectors a bit.

--- sponsoredPostKiller.user.js
+++ sponsoredPostKiller.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         Sponsored Post Killer
 // @namespace    http://lepermessiah.net
-// @version      0.2
+// @version      0.4
 // @description  Hide posts on your dashboard by blogs you don't follow
-// @match        https://*.tumblr.com/dashboard/*
+// @match        https://*.tumblr.com/dashboard*
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.2.1.min.js
 // ==/UserScript==
 
 (function($) {
-    $(".post_container").each(function() {
-        var postdata = $(this).find("div.post").data("json");
+    $("div.post.not_mine").each(function() {
+        var postdata = $(this).data("json");
         if(typeof postdata != 'undefined' && postdata.hasOwnProperty('tumblelog-data')) {
             if(!postdata['tumblelog-data'].following) {
                 $(this).hide();
