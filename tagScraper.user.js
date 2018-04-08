309ec5a1f4c5cd0a8cd3139900d39c4f436f2728
baroquenroll <johannsuggestionbachs@runbox.com>
2018-04-08 13:35:14 -0400

    Add tag scraper script

---
+++ tagScraper.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name Tumblr tag scraper
+// @namespace https://github.com/baroquenroll
+// @description Export the post HTML of a Tumblr tag page
+// @include https://www.tumblr.com/tagged/*
+// @include https://tumblr.com/tagged/*
+// @require http://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js
+// @grant none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var exportTextArea = "<textarea rows='10' cols='86' class='exportForm'>";
+    $("li.post_container > div.post > div.post_wrapper").not('.new_post_buttons_container').each(function(idx) {
+        exportTextArea += "\n\n" + $(this).html() + "\n\n";
+    });
+    $("#posts").before(exportTextArea);
+})();
